a76d4d3cade25d4ff986394e7496a610de624322
gc-plp <plp.github@gmail.com>
2020-05-16 23:41:56 +0000

    update dockerstart examples

--- dockerstart.sh
+++ dockerstart.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 docker run -v musicbot:/bot gcplp/musicbot:latest
+
+# With an options.ini and autoplaylist.txt file prepared, run:
+docker run \
+    -v musicbot:/bot \
+    -v $(pwd)/options.ini:/bot/MusicBot/config/options.ini \
+    -v $(pwd)/autoplaylist.txt:/bot/MusicBot/config/autoplaylist.txt \
+    gcplp/musicbot:latest
